8ef61cae7fcaea3ea56c0e5ac19c5f48278932b1
Rafael Fonseca <rafael@vendhq.com>
2016-04-27 16:53:35 +1200

    Fix a couple of warnings

--- files/main.sh
+++ files/main.sh
@@ -23,19 +23,25 @@ if [ -z "${cmd}" ]; then
     exit 1
 fi
 
+if [ -z "$s3cmd_options" ]; then
+  S3CMD_OPTIONS=""
+else
+  S3CMD_OPTIONS=$s3cmd_options
+fi
+
 #
-# Replace key and secret in the /.s3cfg file with the one the user provided
+# Replace key and secret in the /opt/.s3cfg file with the one the user provided
 #
-echo "" >> /.s3cfg
-echo "access_key=${aws_key}" >> /.s3cfg
-echo "secret_key=${aws_secret}" >> /.s3cfg
+echo "" >> /opt/.s3cfg
+echo "access_key=${aws_key}" >> /opt/.s3cfg
+echo "secret_key=${aws_secret}" >> /opt/.s3cfg
 
 #
 # Add region base host if it exist in the env vars
 #
-if [ ${s3_host_base} != "" ]; then
-  sed -i "s/host_base = s3.amazonaws.com/# host_base = s3.amazonaws.com/g" /.s3cfg
-  echo "host_base = ${s3_host_base}" >> /.s3cfg
+if [ "${s3_host_base}" != "" ]; then
+  sed -i "s/host_base = s3.amazonaws.com/# host_base = s3.amazonaws.com/g" /opt/.s3cfg
+  echo "host_base = ${s3_host_base}" >> /opt/.s3cfg
 fi
 
 #
@@ -43,14 +49,14 @@ fi
 #
 if [ "${cmd}" = "sync-s3-to-local" ]; then
     echo ${src-s3}
-    ${S3CMD_PATH} --config=/.s3cfg  sync ${SRC_S3} /opt/dest/
+    ${S3CMD_PATH} ${S3CMD_OPTIONS} --config=/opt/.s3cfg sync ${SRC_S3} /opt/dest/
 fi
 
 #
 # sync-local-to-s3 - copy from local to s3
 #
 if [ "${cmd}" = "sync-local-to-s3" ]; then
-    ${S3CMD_PATH} --config=/.s3cfg sync /opt/src/ ${DEST_S3}
+    ${S3CMD_PATH} ${S3CMD_OPTIONS} --config=/opt/.s3cfg sync /opt/src/ ${DEST_S3}
 fi
 
 #
